ecd06b898ad5780226e20f8f973b0369ce9a7486
Adam-Shea <adam.shea@ibm.com>
2024-11-20 16:14:40 +0000

    layout basic python api

---
+++ src/main.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+#
+@app.route("/") # Route
+def hello_world():
+    return "<p>Hello, World!</p>" # Response
+
+@app.route("/sql")
+def sql_example():
+    return "<p>Hello, World!</p>"
