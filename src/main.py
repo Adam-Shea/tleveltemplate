fee8f938b4a42ce498620d552cc33900f523e86c
Adam-Shea <adam.shea@ibm.com>
2024-11-20 16:19:32 +0000

    update styling

--- src/main.py
+++ src/main.py
@@ -2,10 +2,13 @@ import sqlite3
 from flask import Flask
 
 flask_app = Flask(__name__)
+sql_connection = sqlite3.connect("")
 
-@flask_app.route("/") # Route
+
+@flask_app.route("/")  # Route
 def hello_world():
-    return "<p>Hello, World!</p>" # Response
+    return "<p>Hello, World!</p>"  # Response
+
 
 @flask_app.route("/sql")
 def sql_example():
